374857cdb0277d5ce71f3553d7c59dc295604aec
sadyanarami <klenovdd@gmail.com>
2025-03-18 23:06:52 +0300

    test commit from pc

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from random import randint
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,8 @@ app = FastAPI()
 @app.get("/")
 async def main():
     return {"massage": "Hello world!"}
+
+@app.get("/pool")
+async def pool():
+    rand_num = randint(0, 10)
+    return {"pool": rand_num}
